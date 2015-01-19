9a4369b4d3cb7ee21a82edfdf7a204fdf6db093c
Nate Olson <nathandavidolson@gmail.com>
2015-01-18 21:01:22 -0500

    added site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'http://nate-d-olson.github.io/lab_notebok/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
